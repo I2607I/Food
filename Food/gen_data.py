21c3f1699389f8db8173c5f548a331a98a9e2094
Игорь Половко <bujhm2607@yandex.ru>
2023-12-26 02:42:43 +0700

    added data generation

--- Food/gen_data.py
+++ Food/gen_data.py
@@ -1,14 +1,13 @@
 from random import choice
-prefix1 = ['Beauty', 'Big', 'Brave', 'Bright', 'Charming', 'Cold', 'Cool', 'Exciting', 'Fresh', 'Gorgeous', 'Hot', 'Huge', 'Important', 'Kind', 'Leading', 'Light', 'Lucky', 'Marvel', 'Nice', 'Old', 'Perfect', 'Rare', 'Safe', 'Small', 'Steel', 'Stone', 'Strange', 'Sweet', 'Tiny', 'True', 'Warm', 'Wise', 'Wonderful']
-prefix2 = ["Black", "White", "Red", "Green", "Yellow", "Blue", "Brown", "Orange", "Pink", "Purple", "Grey"]
-root = ['Book', 'Car', 'Castle', 'City', 'Day', 'Door', 'Game', 'Hand', 'Head', 'Home', 'House', 'Level', 'Line', 'Mind', 'Moment', 'Morning', 'Name', 'Night', 'Number', 'Paper', 'Part', 'Point', 'Power', 'Process', 'Result', 'Sense', 'Side', 'Story', 'Table', 'Team', 'Water', 'Way', 'Week', 'Word', 'World', 'Year']
-for i in range(10):
-    print(choice(prefix1) + choice(prefix2) + choice(root))
 
+def company_name():
+    prefix1 = ['Beauty', 'Big', 'Brave', 'Bright', 'Charming', 'Cold', 'Cool', 'Exciting', 'Fresh', 'Gorgeous', 'Hot', 'Huge', 'Important', 'Kind', 'Leading', 'Light', 'Lucky', 'Marvel', 'Nice', 'Old', 'Perfect', 'Rare', 'Safe', 'Small', 'Steel', 'Stone', 'Strange', 'Sweet', 'Tiny', 'True', 'Warm', 'Wise', 'Wonderful']
+    prefix2 = ["Black", "White", "Red", "Green", "Yellow", "Blue", "Brown", "Orange", "Pink", "Purple", "Grey"]
+    root = ['Book', 'Car', 'Castle', 'City', 'Day', 'Door', 'Game', 'Hand', 'Head', 'Home', 'House', 'Level', 'Line', 'Mind', 'Moment', 'Morning', 'Name', 'Night', 'Number', 'Paper', 'Part', 'Point', 'Power', 'Process', 'Result', 'Sense', 'Side', 'Story', 'Table', 'Team', 'Water', 'Way', 'Week', 'Word', 'World', 'Year']
+    return choice(prefix1) + choice(prefix2) + choice(root)
 
-vowel = ['a', 'e', 'i', 'o', 'u']
-consonant = [chr(ord('a')+ i) for i in range(26) if chr(ord('a')+ i) not in vowel]
-obr = "LIPTON"
-for i in range(10):
+def brand_name():
+    vowel = ['a', 'e', 'i', 'o', 'u']
+    consonant = [chr(ord('a')+ i) for i in range(26) if chr(ord('a')+ i) not in vowel]
     res = choice(consonant) + choice(vowel) + choice(consonant) + choice(consonant) + choice(vowel) + choice(consonant)
-    print(''.join(res))
+    return ''.join(res)
