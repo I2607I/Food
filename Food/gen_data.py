4e7a19d09d2d88ebe9f7c1953545ed12965d54a3
Игорь Половко <bujhm2607@yandex.ru>
2023-12-26 02:44:23 +0700

    added data generation

--- Food/gen_data.py
+++ Food/gen_data.py
@@ -1,13 +1,17 @@
 from random import choice
 
-def company_name():
+def brand_name1():
     prefix1 = ['Beauty', 'Big', 'Brave', 'Bright', 'Charming', 'Cold', 'Cool', 'Exciting', 'Fresh', 'Gorgeous', 'Hot', 'Huge', 'Important', 'Kind', 'Leading', 'Light', 'Lucky', 'Marvel', 'Nice', 'Old', 'Perfect', 'Rare', 'Safe', 'Small', 'Steel', 'Stone', 'Strange', 'Sweet', 'Tiny', 'True', 'Warm', 'Wise', 'Wonderful']
     prefix2 = ["Black", "White", "Red", "Green", "Yellow", "Blue", "Brown", "Orange", "Pink", "Purple", "Grey"]
     root = ['Book', 'Car', 'Castle', 'City', 'Day', 'Door', 'Game', 'Hand', 'Head', 'Home', 'House', 'Level', 'Line', 'Mind', 'Moment', 'Morning', 'Name', 'Night', 'Number', 'Paper', 'Part', 'Point', 'Power', 'Process', 'Result', 'Sense', 'Side', 'Story', 'Table', 'Team', 'Water', 'Way', 'Week', 'Word', 'World', 'Year']
     return choice(prefix1) + choice(prefix2) + choice(root)
 
-def brand_name():
+def brand_name2():
     vowel = ['a', 'e', 'i', 'o', 'u']
     consonant = [chr(ord('a')+ i) for i in range(26) if chr(ord('a')+ i) not in vowel]
     res = choice(consonant) + choice(vowel) + choice(consonant) + choice(consonant) + choice(vowel) + choice(consonant)
     return ''.join(res)
+
+def product_name():
+    name = []
+    type = []
